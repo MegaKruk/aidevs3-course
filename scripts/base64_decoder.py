019f47e612b75fe0fb45fc4f2e1d18a1ab356ef5
MegaKruk <megakruk7@gmail.com>
2025-05-23 13:24:35 +0200

    added clean flag printing to base_64_decoder.py

--- scripts/base64_decoder.py
+++ scripts/base64_decoder.py
@@ -1,4 +1,4 @@
-import base64, itertools
+import base64, itertools, re
 
 CIPH = "GhUiPj1fKTM3NCY1KSUmNxkP"
 KEY  = "ANDRZEJ".encode()
@@ -7,3 +7,7 @@ if __name__=="__main__":
     data = base64.b64decode(CIPH)
     pt = bytes(c ^ k for c, k in zip(data, itertools.cycle(KEY)))
     print(pt)
+    clean = ''.join(chr(b) for b in pt if 32 <= b < 127)  # drukowalne ASCII
+    m = re.search(r'flg[:\s\[{]*(\w+)', clean, re.I)
+    flag = m.group(1).upper()
+    print(flag)
